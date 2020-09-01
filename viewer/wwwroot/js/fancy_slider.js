b863c4378a81457322c96983a90edb30c281ffdb
Otsuka Hiroshi <anagura@gmail.com>
2020-09-01 21:31:24 +0900

    FancySlider部品化中

--- viewer/wwwroot/js/fancy_slider.js
+++ viewer/wwwroot/js/fancy_slider.js
@@ -167,20 +167,4 @@ blockASafterClick - boolean. If user clicked any sliding control, autosliding wo
 */
 window.initialize_slider = () => {
     fncSlider(".example-slider", { autoSlidingDelay: 4000 });
-
-    var $demoCont = document.querySelector(".demo-cont");
-
-    [].slice.call(document.querySelectorAll(".fnc-slide__action-btn")).forEach(function ($btn) {
-        $btn.addEventListener("click", function () {
-            $demoCont.classList.toggle("credits-active");
-        });
-    });
-
-    document.querySelector(".demo-cont__credits-close").addEventListener("click", function () {
-        $demoCont.classList.remove("credits-active");
-    });
-
-    document.querySelector(".js-activate-global-blending").addEventListener("click", function () {
-        document.querySelector(".example-slider").classList.toggle("m--global-blending-active");
-    });
 }
