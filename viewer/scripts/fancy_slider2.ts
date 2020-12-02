1c26f65d9792fe448aa67219f69393858efa9108
otsuka hiroshi <anagura@gmail.com>
2020-12-02 21:16:20 +0900

    typescript化中

--- viewer/scripts/fancy_slider2.ts
+++ viewer/scripts/fancy_slider2.ts
@@ -1,15 +1,45 @@
 ﻿interface Window {
     initialize_slider: any
 }
-
 /* Slider initialization
 options:
 autoSliding - boolean
 autoSlidingDelay - delay in ms. If audoSliding is on and no value provided, default value is 5000
 blockASafterClick - boolean. If user clicked any sliding control, autosliding won't start again
 */
+type options = {
+    autoSlidingDelay: number,
+    isInitialize: boolean,
+}
+
+class fancySlider {
+
+    private static _instance: fancySlider;
+
+    private static getInstance(): fancySlider
+    {
+        if (!this._instance)
+            this._instance = new fancySlider();
+
+        return this._instance;
+    }
+
+    public static initialize = (sliderSelector: string, options: options) => {
+        const sliders: HTMLCollectionOf<Element> = document.getElementsByClassName(sliderSelector);
+
+        Array(sliders).forEach((slider: HTMLCollection) => {
+            fancySlider.getInstance().init(slider, options);
+        });
+    }
+
+    private init = (slider: HTMLCollection, options: options) => {
+        console.log(slider);
+        console.log(options);
+    }
+}
+
 window.initialize_slider = (isInitialize: boolean) => {
- //   fncSlider(".example-slider", { autoSlidingDelay: 4000, isInitialize: isInitialize });
     console.log('initialize_slider');
+    fancySlider.initialize("example-slider", { autoSlidingDelay: 4000, isInitialize: isInitialize });
 }
 
